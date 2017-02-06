b3bbb8dba73d944c817ba8448b1b094be7c92137
Catalin Ionescu <catalin.ionescu282@gmail.com>
2017-02-06 11:25:09 +0000

    [led-from-server] Make the led be in sync with the server

--- client/client.py
+++ client/client.py
@@ -1,6 +1,7 @@
 import os
 import json
 import time
+import commands
 from osHelpers import getMacAddress, getIpAddress
 
 
@@ -9,51 +10,27 @@ url  = 'http://captest.ngrok.io/api/board.json'
 localUrl = "http://192.168.0.128/arduino/digital/13/"
 
 def setPin(ip, pin, value):
-    command ="curl " + ip + "/arduino/digital/" + str(pin) + "/" + str(int(value))
-    print command
-    os.system( command)
+  command ="curl http://root:doghunter@" + ip + "/arduino/digital/" + str(pin) + "/" + str(int(value))
+  print command
+  os.system( command)
 
 def blink(ip, mac):
-    global ledValue
-    time.sleep(1)
-    ledValue = not ledValue
-    setPin(ip, 13, ledValue)
+  global ledValue
+  time.sleep(1)
+  ledValue = not ledValue
+  setPin(ip, 13, ledValue)
 
-def communicate():
-    global ledValue
-    global url
-
-    time.sleep(3)
-    ledValue = not ledValue
-
-
-
-    body = os.system("curl " + url)
-    # print body
-    # resp = json.loads(body)
-    # print resp
-
-    # print error
-
-    # if r["ok"] == False:
-    #     continue
-
-    # print r
-    # print "posting..."
-    # p = requests.post('http://captest.ngrok.io/api/board', data = {'led':True})
-
-    os.system("curl -X POST -d mac='B4:21:8A:F5:0E:A4' -d button="+str(ledValue) + " " + url)
-
-
-
+def communicate(ip):
+  _, response =  commands.getstatusoutput("curl -s " + url)
+  pin_status = json.loads(response)["led"]
+  print "Setting the pin to %s..\n" % pin_status
+  setPin(ip, 13, pin_status)
 
 if __name__ == "__main__":
-    ip = getIpAddress()
-    mac = getMacAddress()
+  ip = getIpAddress()
+  mac = getMacAddress()
 
-    print "IP:", ip
-    print "MAC:", mac
-    while True:
-        blink(ip, mac)
-
-# curl -X PUT -d status="$status" -d result="$(tail -n 100 result.out)" $URL
+  print "IP:", ip
+  print "MAC:", mac
+  while True:
+      communicate(ip)
