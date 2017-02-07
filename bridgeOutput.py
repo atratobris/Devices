8e519f82ca48ed31e5a7901b60b905261c984903
Paul Cristian <pp13003@my.bristol.ac.uk>
2017-02-07 12:43:37 +0000

    working led with bridge

--- bridgeOutput.py
+++ bridgeOutput.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
-import sys
+import commands, json, sys
 sys.path.insert(0, '/usr/lib/python2.7/bridge')
 
 from time import sleep
 from bridgeclient import BridgeClient as bridgeclient
 
-url  = 'http://captest.ngrok.io/api/board.json'
+url  = 'http://caplatform.herokuapp.com/api/board'
 
 # cl: bridgeclient instance
 def blink(cl):
@@ -17,22 +17,27 @@ def blink(cl):
     cl.put('D13','0')
     sleep(0.1)
 
-print("I hope you enjoyed the light show\n")
-
 # value: 0 or 1
 # pin: integer
 # cl: bridgeclient instance
 def setPin(cl, pin, value):
-    value.put('D'+str(pin), str(value))
+    cl.put('D'+str(pin), str(int(value)))
 
 # cl: bridgeclient instance
 def communicate(cl):
-    _, response =  commands.getstatusoutput("curl -s " + url)
+    global url
+    error, response =  commands.getstatusoutput("curl -s " + url)
+    if error:
+        print "error on curl"
+        return
+
+    # print response
     pin_status = json.loads(response)["led"]
-    print "Setting the pin to %s..\n" % pin_status
-    setPin(ip, 13, pin_status)
+    # print "Setting the pin to %s..\n" % pin_status
+    setPin(cl, 13, bool(pin_status))
 
 if __name__=="__main__":
     b_client = bridgeclient()
     while True:
-        blink(b_client)
+        communicate(b_client)
+        sleep(0.1)
