8c2284ad1618e17c85a798912a43aa4bb8927b6f
Catalin Ionescu <catalin.ionescu282@gmail.com>
2017-02-10 12:32:34 +0000

    [websockets] Add a config class and pass along the mac

--- websocketsOutput.py
+++ websocketsOutput.py
@@ -1,20 +1,64 @@
 #!/usr/bin/python
-
-import thread
-import time
-import json, sys
-
-
+import sys
 sys.path.insert(0, '/usr/lib/python2.7/websocket')
 sys.path.insert(0, '/usr/lib/python2.7/bridge')
-from bridgeclient import BridgeClient as bridgeclient
-import websocket
+import thread, time, json, os, websocket, commands, re
+b_client = None
 
-b_client = bridgeclient()
+class Config(object):
+    user = os.environ.get('USER')
+    operating_system = sys.platform
+    mac = None
+    @classmethod
+    def embedded(cls):
+        return cls.user == "root"
+    @classmethod
+    def getOs(cls):
+        return cls.operating_system
+    @classmethod
+    def getMac(cls):
+        if cls.mac == None:
+            cls.mac = cls.getMacAddress()
+        if cls.getOs() == 'darwin':
+            return "1234"
+        else:
+            return cls.mac
+    @classmethod
+    def getMacAddress(cls):
+        wlanInfo = cls.getWlanInfo()
+        if wlanInfo == "Error":
+            return
+        elif wlanInfo:
+            regex = ".*HWaddr (.*)"
+            lines = wlanInfo.split("\n")
+            for line in lines:
+                matched = re.match(regex, line)
+                if matched:
+                    return matched.group(1)
+        return ""
+    @classmethod
+    def getWlanInfo(cls):
+        status, output = commands.getstatusoutput("ifconfig")
+        if status == 0:
+            for connection in output.split("\n\n"):
+                if cls.checkIfWlan(connection):
+                    return connection
+        else:
+            return "Error"
+    @classmethod
+    def checkIfWlan(cls, text):
+        regex = "wlan0.*"
+        matched = re.match(regex, str(text))
+        if matched:
+            return True
+        return False
 
-# value: 0 or 1
-# pin: integer
-# cl: bridgeclient instance
+if Config.embedded():
+    from bridgeclient import BridgeClient as bridgeclient
+    b_client = bridgeclient()
+
+
+# value: 0 or 1 | pin: integer | cl: bridgeclient instance
 def setPin(cl, pin, value):
     cl.put('D'+str(pin), str(int(value)))
 
@@ -32,13 +76,14 @@ def on_message(ws, message):
     # transform string to json
     message = json.loads(message)
 
-    # this is an activ message from the server
+    # this is an active message from the server
     if "message" in message and "identifier" in message:
         identifier = json.loads(message["identifier"])
         data = message["message"]
         print "{} from {}".format( data["message"], identifier["channel"])
         pin_status = bool(data["message"])
-        setPin(b_client, 13, bool(pin_status))
+        if Config.embedded():
+            setPin(b_client, 13, bool(pin_status))
         return
 
     # get type of message
@@ -67,7 +112,8 @@ def on_close(ws):
 def on_open(ws):
     def run(*args):
         identifier = {
-            "channel":"SketchChannel"
+            "channel":"SketchChannel",
+            "mac": Config.getMac()
         }
         data = {
             "message":"mama",
@@ -89,6 +135,8 @@ def on_open(ws):
     thread.start_new_thread(run, ())
 
 if __name__ == "__main__":
-    ws = set_web_socket("ws://caplatform.herokuapp.com/cable")
-    # ws = set_web_socket("ws://localhost:3000/cable")
+    if Config.embedded():
+        ws = set_web_socket("ws://caplatform.herokuapp.com/cable")
+    else:
+        ws = set_web_socket("ws://localhost:3000/cable")
     ws.run_forever()
