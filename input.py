595f7e24f4a36963a22b4facc4921108503e19e6
Catalin Ionescu <catalin.ionescu282@gmail.com>
2017-02-14 12:42:31 +0000

    [input] Simulate input

--- input.py
+++ input.py
@@ -1,16 +1,61 @@
 #!/usr/bin/python
-import logging
-import logging.handlers
-import traceback
-import time
-import os
+import logging, logging.handlers, traceback, time, os, json, sys, commands, re
 from socket import socket, AF_INET, SOCK_STREAM
-import json, sys
 
 sys.path.insert(0, '/usr/lib/python2.7/websocket')
 import websocket
 ###################### LIBRARIES ######################
 ################## COMPILED TOGETHER ##################
+
+DEFAULT_MAC = "1234"
+
+class Config(object):
+    user = os.environ.get('USER')
+    operating_system = sys.platform
+    mac = None
+    @classmethod
+    def embedded(cls):
+        return cls.user == "root"
+    @classmethod
+    def getOs(cls):
+        return cls.operating_system
+    @classmethod
+    def getMac(cls):
+        if cls.mac == None:
+            cls.mac = cls.getMacAddress()
+        if cls.getOs() == 'darwin':
+            return DEFAULT_MAC
+        else:
+            return cls.mac
+    @classmethod
+    def getMacAddress(cls):
+        wlanInfo = cls.getWlanInfo()
+        if wlanInfo == "Error":
+            return
+        elif wlanInfo:
+            regex = ".*HWaddr (.*)"
+            lines = wlanInfo.split("\n")
+            for line in lines:
+                matched = re.match(regex, line)
+                if matched:
+                    return matched.group(1)
+        return ""
+    @classmethod
+    def getWlanInfo(cls):
+        status, output = commands.getstatusoutput("ifconfig")
+        if status == 0:
+            for connection in output.split("\n\n"):
+                if cls.checkIfWlan(connection):
+                    return connection
+        else:
+            return "Error"
+    @classmethod
+    def checkIfWlan(cls, text):
+        regex = "wlan0.*"
+        matched = re.match(regex, str(text))
+        if matched:
+            return True
+        return False
 
 class SERIAL:
   class MSG:
@@ -19,7 +64,7 @@ class SERIAL:
     END             = chr(31)
 
 class Logger:
-  if os.environ.get('USER') == 'root':
+  if Config.embedded():
     file_name = '/root/YunMessenger.log'
   else:
     file_name = './YunMessenger.log'
@@ -128,24 +173,17 @@ def button_handler(msg):
     global ws
     global ledOn
     print 'Button Handler received %s..' % msg
-    if not ledOn:
-        os.system("curl -X POST -d mac='B4:21:8A:F8:2E:23' -d button=True " + url)
-        ledOn = not ledOn
-        button = True
-    else:
-        os.system("curl -X POST -d mac='B4:21:8A:F8:2E:23' -d button=False " + url)
-        ledOn = not ledOn
-        button = False
-    ws.send(ws_message(button, "SketchChannel"))
+    ws.send(ws_message(True, "SketchChannel"))
 
 
 def greetings(channel_name):
     identifier = {
-        "channel":channel_name
+      "channel": channel_name,
+      "mac": Config.getMac()
     }
     regards = {
-        "command": "subscribe",
-        "identifier": json.dumps(identifier)
+      "command": "subscribe",
+      "identifier": json.dumps(identifier)
     }
     return json.dumps(regards)
 
@@ -153,11 +191,12 @@ def greetings(channel_name):
 
 def ws_message(data_input, channel_name):
     identifier = {
-        "channel":channel_name
+      "channel":channel_name,
+      "mac": Config.getMac()
     }
     data = {
-        "message": data_input,
-        "action": "blink"
+      "message": data_input,
+      "action": "blink"
     }
     message = {
         "command": "message",
@@ -167,10 +206,20 @@ def ws_message(data_input, channel_name):
     return json.dumps(message)
 
 if __name__ == '__main__':
-  global ws
-  ws = websocket.create_connection("ws://caplatform.herokuapp.com/cable")
+  if Config.embedded():
+    ws_url = "ws://caplatform.herokuapp.com/cable"
+  else:
+    ws_url = "ws://localhost:3000/cable"
+  ws = websocket.create_connection(ws_url)
   ws.send(greetings("SketchChannel"))
   time.sleep(1)
 
-  console.onMessage['button_pressed'] = button_handler
-  console.run()
+  if Config.embedded():
+    console.onMessage['button_pressed'] = button_handler
+    console.run()
+  else:
+    idx = 0
+    while idx < 100:
+      button_handler("Blah")
+      time.sleep(1)
+      idx += 1
